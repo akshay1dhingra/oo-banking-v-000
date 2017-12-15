b439ff8b1be12a54363d0dd30ecdd63b87239be7
Akshay Dhingra <akshay1dhingra@gmail.com>
2017-12-15 05:08:01 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -34,7 +34,7 @@ class Transfer
   end
 
   def reject_transfer
-    self.status = "Transaction rejected. Please check your account balance."
+    self.status = "rejected"
     "Transaction rejected. Please check your account balance."
   end
 end
