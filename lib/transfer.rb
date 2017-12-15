40211f4b669771bffed875c993e519c61c7acfe6
Akshay Dhingra <akshay1dhingra@gmail.com>
2017-12-15 05:08:22 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,7 +24,7 @@ class Transfer
   end
 
   def reverse_transfer
-    if valid? && receiver.balance > amount && self.status == "completed"
+    if valid? && receiver.balance < amount && self.status == "completed"
       receiver.balance -= amount
       sender.balance += amount
       self.status = "pending"
